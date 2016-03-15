39b85630e89c8a70e1560907701ec359449c5558
luis <luis@luis-Satellite-C40-A>
2016-03-15 17:10:36 -0600

    Restructurando el proyecto

--- Practica1Is1/src/java/test/Main.java
+++ Practica1Is1/src/java/test/Main.java
@@ -1,9 +1,10 @@
 package test;
 
 import Mapeo.Usuario;
-import Mapeo.Contrasenia;
+import Mapeo.Login;
 import controlador.UsuarioDaoHibernate;
-import controlador.ContraseniaDaoHibernate;
+import controlador.LoginDaoHibernate;
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
@@ -15,19 +16,23 @@ public class Main {
 
     public static void main(String[] args) {
         UsuarioDaoHibernate usuarioDAO = new UsuarioDaoHibernate();
-        ContraseniaDaoHibernate contraseniaDAO = new ContraseniaDaoHibernate();
+        LoginDaoHibernate loginDao = new LoginDaoHibernate();
+        Date fecha = new Date(2016,3,15);
+        Login login = new Login();
         Usuario usuario = new Usuario();
-        Contrasenia contrasenia = new Contrasenia();
-        usuario.setIdUsuario(2);
+        usuario.setIdUsuario(4);
         usuario.setNombre("Juan");
-        usuario.setApp("Perez");
-        usuario.setMail("cuenta2@gmail.com");
+        usuario.setApellidos("Perez");
+        usuario.setCorreo("cuenta2@gmail.com");
         usuarioDAO.save(usuario);
         System.out.println("Todo salio bien en el insert");
         usuario.setNombre("Caen");
         usuarioDAO.update(usuario);
         System.out.println("Todo salio bien en el update");
-
+        login.setIdLog(1);
+        login.setUsuario(usuario);
+        login.setFecha(fecha);
+        loginDao.save(login);
         List<Usuario> lista = usuarioDAO.findAll();
         for (Usuario user : lista) {
             System.out.println(user.toString());
